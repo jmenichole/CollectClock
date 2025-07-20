7295f0d0eaac088c957fba6c50223f5a0ff42e40
Jamie Vargas <j.chapman7@yahoo.com>
2025-07-20 17:22:06 -0500

    Delete collectclock/CollectClock directory

--- collectclock/CollectClock/backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const activeUsersRoute = require('./routes/activeUsers');
-const statisticsRoute = require('./routes/statistics');
-
-app.use('/api/active-users', activeUsersRoute);
-app.use('/api/statistics', statisticsRoute);
