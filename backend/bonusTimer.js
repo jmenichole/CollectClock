4a2b1cb77c29f8f19dd8025f258feafa8220d3c6
Jamie Vargas <j.chapman7@yahoo.com>
2025-07-20 17:05:00 -0500

    Delete backend directory

--- backend/bonusTimer.js
+++
@@ -1,9 +0,0 @@
-const BONUS_INTERVAL = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-
-function getNextBonusTime(lastCollectedTime, interval = BONUS_INTERVAL) {
-    const now = Date.now();
-    const nextBonusTime = lastCollectedTime + interval;
-    return nextBonusTime > now ? nextBonusTime : now + interval;
-}
-
-module.exports = { getNextBonusTime };
